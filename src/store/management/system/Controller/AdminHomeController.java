5de957c32f86d5c9cc4bf8836e7ceed15e83f4ac
Subham Adhikari <subhamadhikari20@gmail.com>
2024-08-17 22:57:29 +0545

    Adding home page

---
+++ src/store/management/system/Controller/AdminHomeController.java
@@ -0,0 +1,50 @@
+
+package store.management.system.Controller;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.EmptyBorder;
+
+// Modules
+import store.management.system.View.AdminHomeView;
+//import store.management.system.DAO.AdminHomeDAO;
+//import store.management.system.Model.AdminHomeModel;
+import store.management.system.View.AdminStaffView;
+
+public class AdminHomeController {
+    private AdminHomeView adminHomeView;
+//    private AdminStaffView adminStaffView;
+    
+    public AdminHomeController(AdminHomeView adminHomeView){
+        this.adminHomeView = adminHomeView;
+        this.adminHomeView.addHomeMenuButtonListner(new HomeMenuButtonListener());
+        this.adminHomeView.addStaffMenuButtonListner(new StaffMenuButtonListener());
+        this.adminHomeView.addInvoiceMenuButtonListner(new InvoiceMenuButtonListener());
+    }
+    
+    class HomeMenuButtonListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            adminHomeView.getCardLayout().show(adminHomeView.getMainPanel(), "Home");
+        }
+        
+    }
+    
+    class StaffMenuButtonListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            adminHomeView.getCardLayout().show(adminHomeView.getMainPanel(), "Staff");
+        }
+    }
+    
+    class InvoiceMenuButtonListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            adminHomeView.getCardLayout().show(adminHomeView.getMainPanel(), "Invoice");
+        }
+    }
+    
+    
+   
+}
